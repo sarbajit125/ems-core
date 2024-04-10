50823226cd687c3dc5f1e46f3607338b7017c714
Sarbajit Biswal <sarbajit.biswal@comviva.com>
2024-04-10 15:08:08 +0530

    added home bottom view

--- app/screens/homeone.tsx
+++ app/screens/homeone.tsx
@@ -1,35 +1,95 @@
-
-import React, { useMemo } from 'react'
-import { View, StyleSheet, Text } from 'react-native';
-import { useTheme, MD3Theme } from 'react-native-paper';
+import React, { useMemo } from "react";
+import { View, StyleSheet, Image } from "react-native";
+import { useTheme, MD3Theme, Button, Text } from "react-native-paper";
 
 function HomeOneScreen() {
-    const theme = useTheme();
-    const styles = useMemo(() => createStyles(theme), [theme]);
+  const theme = useTheme();
+  const styles = useMemo(() => createStyles(theme), [theme]);
   return (
     <View style={styles.container}>
       <View style={styles.topView}></View>
       <View style={styles.serviceView}></View>
-      <View style={styles.bottomView}></View>
+      <View style={styles.bottomView}>
+        <Image
+          style={styles.bottomBanner}
+          source={{
+            uri: "https://goldeneagle.comviva.com/idm-service/idm/v0/assets/images/Icon_comviva_orb.png",
+          }}
+        />
+        <View>
+          <Text variant="headlineMedium">Refer & Earn!</Text>
+          <Text variant="bodyMedium">Spread the word we'll reward you</Text>
+          <Button mode="contained">Click to share Now</Button>
+        </View>
+      </View>
     </View>
-  )
+  );
 }
 
-export default HomeOneScreen
-const createStyles = (theme: MD3Theme) => StyleSheet.create({
+export default HomeOneScreen;
+const createStyles = (theme: MD3Theme) =>
+  StyleSheet.create({
     container: {
-        flex: 1
+      flex: 1,
     },
     topView: {
-      height: 300,
-      backgroundColor: 'red',
+      height: 200,
+    },
+    navView: {
+      marginTop: 32,
+      flexDirection: "row",
+      padding: 5,
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    navRightView: {
+      flexDirection: "row",
+      padding: 4,
+      alignItems: "baseline",
+    },
+    logo: {
+      width: 140,
+      height: 80,
+      resizeMode: "contain",
     },
     bottomView: {
-      height: 200,
-      backgroundColor: 'blue',
+      height: 140,
+      backgroundColor: "blue",
+      flexDirection: "row",
+    },
+    bottomBanner: {
+      width: 120,
+      resizeMode: "contain",
     },
     serviceView: {
       flex: 1,
-      backgroundColor: 'yellow',
-    }
-})
+      backgroundColor: "yellow",
+    },
+    gridView: {
+      flexDirection: "row",
+      flexWrap: "wrap",
+      flex: 3,
+      gap: 8,
+      padding: 8,
+    },
+    gridItem: {
+      flexDirection: "column",
+      gap: 8,
+      height: 60,
+      padding: 16,
+      margin: 16,
+    },
+    gridItemImage: {
+      width: 50,
+      height: 50,
+    },
+    gridItemName: {
+      width: 50,
+      flexWrap: "wrap",
+    },
+    serviceTitle: {
+      padding: 4,
+      margin: 4,
+      alignSelf: "center",
+    },
+  });
